04db0e8478ace4a3b3f54a0c9bd4117d3d5b1e64
Lukkas98 <lucaspalma988@gmail.com>
2023-11-27 21:23:12 -0300

    fix animate page

--- app/template.jsx
+++ app/template.jsx
@@ -1,11 +1,45 @@
-import PageAnimate from "./components/pageAnimate";
+"use client";
+
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const variants = {
+  hidden: { opacity: 0, x: -100, y: 0, scale: 0.3, rotate: 25 },
+  enter: { opacity: 1, x: 0, y: 0, scale: 1, rotate: 0 },
+};
 
 export default function TemplateRoot({ children }) {
-    return (
-      <>
-        <main className={`text-white grid items-center h-full`}>
-          <PageAnimate>{children}</PageAnimate>
-        </main>
-      </>
-    );
-  }
+  const isPresent = useIsPresent();
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence>
+      <motion.main
+        key={pathname + "2"}
+        initial="hidden"
+        animate="enter"
+        variants={variants}
+        transition={{ duration: 0.9, type: "linear" }}
+        className={`text-white grid items-center h-full`}
+      >
+        {children}
+      </motion.main>
+      <motion.div
+        key={pathname}
+        initial={{ scaleX: 1, backgroundColor: "#1e293a" }}
+        animate={{
+          scaleX: 0,
+          backgroundColor: "#1e293a",
+          transition: { duration: 0.5, ease: "circIn" },
+        }}
+        exit={{
+          scaleX: 1,
+          backgroundColor: "#9A3412",
+          transition: { duration: 0.5, ease: "circIn" },
+        }}
+        style={{ originX: isPresent ? 0 : 1 }}
+        className="fixed top-0 left-0 right-0 bottom-0 z-50"
+      />
+    </AnimatePresence>
+  );
+}
