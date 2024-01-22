60544792f5319fa426e8587fe589c86160884fe9
Lukkas98 <lucaspalma988@gmail.com>
2024-01-22 15:01:01 -0300

    fix

--- app/components/animates/buttonAnimate.jsx
+++ app/components/animates/buttonAnimate.jsx
@@ -8,8 +8,8 @@ export default function ButtonAnimate({ href = "/", text }) {
     <motion.div
       className="px-2 py-1 cursor-pointer flex items-center justify-center rounded-2xl"
       whileHover={{ scale: 1.3 }}
-      initial={{ color: "white", opacity: 1 }}
-      animate={{ opacity: [0.7, 1] }}
+      initial={{ opacity: 1, scale:1 }}
+      animate={{ opacity: [0.7, 1,], scale:1 }}
       transition={{ duration: 1, repeat: Infinity, repeatType: "mirror" }}
     >
       <Link className="capitalize" href={href}>
