2ee4d590be01f232850c31271624b000e644eb89
Lukkas98 <lucaspalma988@gmail.com>
2024-06-14 16:49:38 -0300

    minor change

--- app/components/animates/buttonAnimate.jsx
+++ app/components/animates/buttonAnimate.jsx
@@ -6,10 +6,9 @@ import Link from "next/link";
 export default function ButtonAnimate({ href = "/", text }) {
   return (
     <motion.div
-      className="px-2 py-1 cursor-pointer flex items-center justify-center rounded-2xl transition-all duration-500 hover:scale-110"
-      initial={{ opacity: 1 }}
-      animate={{ opacity: [0.7, 1,]}}
-      transition={{ duration: 1, repeat: Infinity, repeatType: "mirror" }}
+      className="px-2 py-1 cursor-pointer flex items-center justify-center rounded-2xl"
+      transition={{ duration: 0.4 }}
+      whileHover={{ scale: 1.2, color: "#c2410c"}}
     >
       <Link className="capitalize" href={href}>
         {text}
--- app/components/animates/pageTransition.jsx
+++ app/components/animates/pageTransition.jsx
@@ -1,15 +1,26 @@
 "use client"
 import { motion, useIsPresent } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function PageTransition() {
   const isPresent = useIsPresent();
+  const [initialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    setInitialRender(false);
+  }, []);
+
   return (
-    <motion.div
-      initial={{ scaleX: 1 }}
-      animate={{ scaleX: 0, transition: { duration: 0.5, ease: "circOut" } }}
-      exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
-      style={{ originX: isPresent ? 0 : 1 }}
-      className="privacy-screen"
-    />
+    <>
+      {!initialRender && (
+        <motion.div
+          initial={{ scaleX: 1 }}
+          animate={{ scaleX: 0, transition: { duration: 0.5, ease: "circOut" } }}
+          exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+          style={{ originX: isPresent ? 0 : 1 }}
+          className="privacy-screen"
+        />
+      )}
+    </>
   );
 }
